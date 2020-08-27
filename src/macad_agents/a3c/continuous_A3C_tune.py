04fa8aaf78fcbfe281c434eafa06d221774ff2c0
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2020-08-27 09:05:20 -0700

    Update to import macad_gym instead of env; Fixes #6

--- src/macad_agents/a3c/continuous_A3C_tune.py
+++ src/macad_agents/a3c/continuous_A3C_tune.py
@@ -1,22 +1,18 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import ray
-from ray.tune import register_env, run_experiments, register_trainable
-
-from env.carla.multi_env import MultiCarlaEnv, DEFAULT_MULTIENV_CONFIG
-from macad_agents.rllib.models import register_carla_model
-from env.carla.scenarios import update_scenarios_parameter
-
-from .continuous_A3C_base import ContinuousA3CTune
-
-import os
+from __future__ import absolute_import, division, print_function
 
 import json
-
 import multiprocessing
+import os
+
 import GPUtil
+import ray
+from macad_gym.carla.multi_env import DEFAULT_MULTIENV_CONFIG, MultiCarlaEnv
+from macad_gym.carla.scenarios import update_scenarios_parameter
+from ray.tune import register_env, register_trainable, run_experiments
+
+from macad_agents.rllib.models import register_carla_model
+
+from .continuous_A3C_base import ContinuousA3CTune
 
 cpu = multiprocessing.cpu_count() / 2
 if cpu < 1:
@@ -30,7 +26,8 @@ if gpu_info is not None and len(gpu_info) > 0:
 env_name = "carla_env"
 env_config = DEFAULT_MULTIENV_CONFIG
 config_update = update_scenarios_parameter(
-    json.load(open("macad_agents/a3c/env_config.json")))
+    json.load(open("macad_agents/a3c/env_config.json"))
+)
 env_config.update(config_update)
 
 register_env(env_name, lambda env_config: MultiCarlaEnv(env_config))
@@ -57,29 +54,25 @@ last_checkpoint = None
 
 MAX_EP = 10000000  # 10M
 
-run_experiments({
-    "carla-continuous-a3c": {
-        "run": "continuous_A3C",
-        "env": "carla_env",
-        "trial_resources": {
-            "cpu": cpu,
-            "gpu": gpu,
-            "extra_gpu": 0
+run_experiments(
+    {
+        "carla-continuous-a3c": {
+            "run": "continuous_A3C",
+            "env": "carla_env",
+            "trial_resources": {"cpu": cpu, "gpu": gpu, "extra_gpu": 0},
+            "stop": {"training_iteration": MAX_EP},
+            "config": {
+                "env_config": env_config,
+                "gamma": 0.9,
+                "num_workers": 1,
+                "num_local_workers": 3,
+                "save_checkpoint_path": save_model_dir,
+                "load_checkpoint_path": last_checkpoint,
+                "MAX_EP": MAX_EP,
+                "MAX_EP_STEP": 2000,
+                "SAVE_STEP": 2000000,
+                "UPDATE_GLOBAL_ITER": 5,
+            },
         },
-        "stop": {
-            "training_iteration": MAX_EP
-        },
-        "config": {
-            "env_config": env_config,
-            "gamma": 0.9,
-            "num_workers": 1,
-            "num_local_workers": 3,
-            "save_checkpoint_path": save_model_dir,
-            "load_checkpoint_path": last_checkpoint,
-            "MAX_EP": MAX_EP,
-            "MAX_EP_STEP": 2000,
-            "SAVE_STEP": 2000000,
-            "UPDATE_GLOBAL_ITER": 5,
-        },
-    },
-})
+    }
+)
